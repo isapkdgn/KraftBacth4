c189d408fce0c54171663a8f315983b8fddd584c
isapkdgn <isapkdgn@gmail.com>
2023-03-27 22:44:06 +0300

    commit111

---
+++ src/day21_Class_And_Object/C04_Methods_Last_Question.java
@@ -0,0 +1,31 @@
+package day21_Class_And_Object;
+
+import java.util.Scanner;
+
+public class C04_Methods_Last_Question {
+    public static void main(String[] args) {
+        sayiAl();
+
+    }
+    public static void sayiAl(){
+        Scanner scan=new Scanner(System.in);
+        System.out.print("1.sayı :");
+        int sayi= scan.nextInt();
+        System.out.print("2, sayi :");
+        int sayi2= scan.nextInt();
+        int sayi3=ucunkatı(sayi2);
+        System.out.println(sayılariTopla(sayi,ucunkatı(sayi2)));
+    }
+    public static int ucunkatı(int sayi2){
+        if (sayi2%3==0){
+            return sayi2;
+        }
+
+        return  sayi2-sayi2%3;
+    }
+    public static int sayılariTopla(int x,int y ){
+        return x+y;
+    }
+
+
+}
