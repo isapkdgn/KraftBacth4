14bfa2fa8fde27ad892962dc675e7c7af59fe3c6
isapkdgn <isapkdgn@gmail.com>
2023-03-29 13:50:08 +0300

    121

---
+++ src/day22_ClassAndobject_Memory/MainMath2.java
@@ -0,0 +1,9 @@
+package day22_ClassAndobject_Memory;
+
+public class MainMath2 {
+
+
+    public static void main(String[] args) {
+        System.out.println(Math2.math2(2, 3));
+    }
+}
---
+++ src/day22_ClassAndobject_Memory/Math2.java
@@ -0,0 +1,17 @@
+package day22_ClassAndobject_Memory;
+
+public class Math2 {
+
+
+
+    public static int math2(int t,int k){
+        int carpım=1;
+        for (int i=1;i<=k;i++){
+
+            carpım=carpım*t;
+        }
+        return carpım;
+    }
+
+
+}
